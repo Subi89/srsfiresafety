f17f9b4760c016f24cada84f3dc69448ef0850cd
Harisankar Vellingiri <harisankar.vellingiri@echostar.com>
2018-02-25 20:00:59 -0700

    Moved carousel and sidemenu to its own component files and simplified App.js

---
+++ src/components/carousel.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import carouselStyles from '../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css';
+var Carousel = require('react-responsive-carousel').Carousel;
+
+
+class carousel extends Component {
+    render(){
+        return(
+            <div class={carouselStyles.carousel}>
+                <Carousel autoPlay={true} infiniteLoop={true}>
+                    <div>
+                        <img src={require('../images/1.jpg')} />
+                        <p className="legend">Legend 1</p>
+                    </div>
+                    <div>
+                        <img src={require('../images/2.jpg')} />
+                        <p className="legend">Legend 2</p>
+                    </div>
+                    <div>
+                        <img src={require('../images/1.jpg')} />
+                        <p className="legend">Legend 3</p>
+                    </div>
+                    <div>
+                        <img src={require('../images/2.jpg')} />
+                        <p className="legend">Legend 4</p>
+                    </div>
+                    <div>
+                        <img src={require('../images/1.jpg')}/>
+                        <p className="legend">Legend 5</p>
+                    </div>
+                    <div>
+                        <img src={require('../images/2.jpg')} />
+                        <p className="legend">Legend 6</p>
+                    </div>
+                </Carousel>
+            </div>
+        );
+    }
+}
+
+export default carousel;
---
+++ src/components/sidemenu.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import {SideMenu} from 'react-sidemenu';
+import sidemenuStyles from '../../node_modules/react-sidemenu/dist/react-sidemenu.min.css';
+
+
+const items = [
+    {divider: true, label: 'Our Products', value: 'main-nav'},
+    {label: 'Fire Extinguishers', value: 'fireExtinguishers', icon: 'fa-fire-extinguisher',
+        children: [
+            {label: 'Powder Fire Extinguishers', value: 'powderFireExtinguishers'},
+            {label: 'Clean Agent Fire Extinguishers', value: 'cleanAgentFireExtinguishers'},
+            {label: 'Water Fire Extinguishers', value: 'waterFireExtinguishers'},
+            {label: 'Foam Fire Extinguishers', value: 'foamFireExtinguishers'},
+            {label: 'Carbon dioxide Fire Extinguishers', value: 'carbonDioxideFireExtinguishers'},
+            {label: 'Water Mist Fire Extinguishers', value: 'waterMistFireExtinguishers'},
+            {label: 'Backup Trolley Mount Fire Extinguishers', value: 'backupTrolleyFireExtinguishers'},
+            {label: 'Automatic Fire Extinguishers', value: 'automaticFireExtinguishers'}
+        ]
+    },
+    {label: 'Fire Hydrant System', value: 'fireHydrantSystem', icon: 'fa-automobile',
+        children: [
+            {label: 'Yard Fire Hydrant System', value: 'yardFireHydrantSystem'},
+            {label: 'Hose Reel System', value: 'hoseReelSystem'},
+            {label: 'Down Corner System', value: 'downCornerSystem'},
+            {label: 'Dry Riser System', value: 'dryRiserSystem'},
+            {label: 'Wet Riser System', value: 'wetRiserSystem'},
+            {label: 'Automatic Sprinkler System', value: 'automaticSprinklerSystem'}
+        ]
+    },
+    {label: 'Smoke And Fire Detection System', value: 'smokeAndFireDetectionSystem', icon: 'fa-automobile',
+        children: [
+            {label: 'Conventional Fire Alarm System', value: 'conventionalFireAlarmSystem'},
+            {label: 'Addressable Fire Alarm System', value: 'addressableFireAlarmSystem'},
+            {label: 'Stand Alone Fire Alarm System', value: 'standAloneFireAlarmSystem'}
+        ]
+    },
+    {label: 'Escape Signs And Directional Lighting System', value: 'escapeSignsAndDirectionalLightingSystem', icon: 'fa-automobile',
+        children: [
+            {label: 'Photo Luminescent Sign Boards', value: 'conventionalFireAlarmSystem'},
+            {label: 'LED Sign Boards', value: 'ledSignBoards'},
+            {label: 'Normal Sign Boards', value: 'normalSignBoards'},
+            {label: 'Emergency Exit Lights', value: 'emergencyExitLights'}
+        ]
+    },
+    {label: 'Safety Suits', value: 'escapeSignsAndDirectionalLightingSystem', icon: 'fa-automobile',
+        children: [
+            {label: 'Photo Luminescent Sign Boards', value: 'conventionalFireAlarmSystem'},
+            {label: 'LED Sign Boards', value: 'ledSignBoards'},
+            {label: 'Normal Sign Boards', value: 'normalSignBoards'},
+            {label: 'Emergency Exit Lights', value: 'emergencyExitLights'}
+        ]
+    },
+    {label: 'Personal Protective Equipments', value: 'safetySuits', icon: 'fa-automobile',
+        children: [
+            {label: 'Photo Luminescent Sign Boards', value: 'conventionalFireAlarmSystem'},
+            {label: 'LED Sign Boards', value: 'ledSignBoards'},
+            {label: 'Normal Sign Boards', value: 'normalSignBoards'},
+            {label: 'Emergency Exit Lights', value: 'emergencyExitLights'}
+        ]
+    },
+    {label: 'Suppression Systems', value: 'escapeSignsAndDirectionalLightingSystem', icon: 'fa-automobile',
+        children: [
+            {label: 'Photo Luminescent Sign Boards', value: 'conventionalFireAlarmSystem'},
+            {label: 'LED Sign Boards', value: 'ledSignBoards'},
+            {label: 'Normal Sign Boards', value: 'normalSignBoards'},
+            {label: 'Emergency Exit Lights', value: 'emergencyExitLights'}
+        ]
+    },
+    {label: 'Safety Suits', value: 'safetySuits', icon: 'fa-automobile',
+        children: [
+            {label: 'Boiler Suits', value: 'boilerSuits'},
+            {label: 'Industrial Suits', value: 'industrialSuits'},
+            {label: 'Fire Suits', value: 'fireSuits'}
+        ]
+    },
+    {label: 'Personal Protective Equipment', value: 'personalProtetiveEquipment', icon: 'fa-automobile',
+        children: [
+            {label: 'Head Protection', value: 'headProtection'},
+            {label: 'Eye Protection', value: 'eyeProtection'},
+            {label: 'Ear Protection', value: 'earProtection'},
+            {label: 'Face Protection', value: 'faceProtection'},
+            {label: 'Hand Protection', value: 'handProtection'},
+            {label: 'Leg Protection', value: 'legProtection'}
+        ]
+    },
+    {label: 'Suppression System', value: 'suppressionSystem', icon: 'fa-automobile',
+        children: [
+            {label: 'Gas Suppression System', value: 'gasSuppressionSystem'},
+            {label: 'Kitchen Suppression System', value: 'kitchenSuppressionSystem'},
+            {label: 'Water Mist Suppression System', value: 'waterMistSuppressionSystem'}
+        ]
+    }
+];
+
+class sidemenu extends Component {
+    render() {
+        return(
+        <div className={"sidemenuStyles.Side-menu"}>
+            <SideMenu items={items}/>
+        </div>
+        );
+    }
+}
+
+export default sidemenu;
