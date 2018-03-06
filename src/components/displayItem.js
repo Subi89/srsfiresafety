e38d625db71faa209d9a152d02d4072078cbdbf0
Harisankar Vellingiri <harisankar.vellingiri@echostar.com>
2018-03-05 22:09:23 -0700

    Removed modal changes

--- src/components/displayItem.js
+++ src/components/displayItem.js
@@ -1,46 +1,17 @@
 import React, { Component } from 'react';
-import Modal from 'react-responsive-modal';
-import '../custom-animation.css';
 
 const style = {
     background: 'black',
     display: 'inline-block',
-    width: '200px',
-    margin: '13px',
-    padding: '5px',
-    height: '150px',
-    borderRadius: '10px',
-    boxShadow: '0 4px 8px 0 #002B36, 0 6px 20px 0 #002B36',
+    // width: '64vw',
+    margin: '40px',
+    // height: '49vh',
+    borderRadius: '28px',
     position: 'relative',
     overflow: 'auto'
 };
 
-const buttonStyle = {
-	background: '#4CAF50', /* Green */
-    border: 'none',
-    color: 'white',
-    padding: '15px 32px',
-    textAlign: 'center',
-    textDecoration: 'none',    
-	display: 'inline-block',
-    fontSize: '16px',
-    margin: '4px 2px',
-    cursor: 'pointer',
-    webkitTransitionDuration: '0.4s', /* Safari */
-    transitionDuration: '0.4s',
-	boxShadowhadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
-};
-
-const imageStyle = {
-   maxWidth: '100%',
-   maxHeight: '100%'
-};
-
-const captionStyle = {
-    textAlign: 'center'
-}
-
-/*class displayItem extends Component {
+class displayItem extends Component {
 
     constructor(props){
         super(props);
@@ -55,77 +26,11 @@ const captionStyle = {
         let { title } = this.props;
         return(
                 <div style={style}>
-                    <img src={require('../images/background.png')} alt={""}  style={imageStyle} />
-                    <p style={captionStyle}>{title}</p>
-                </div>
-        );
-    }
-}
-
-export default displayItem;*/
-
-
-
-
-
-
-class displayItem extends Component {
-	state = {
-    open: false,
-	//title:[],
-  };
-
-  onOpenModal = () => {
-    this.setState({ open: true });
-  };
-
-  onCloseModal = () => {
-    this.setState({ open: false });
-  };
-
-    
-
-    
-
-    render(){
-		const { open } = this.state;
-        let { title } = this.props;
-		  
-         
-        return(
-                <div style={style}>
                     <img src={require('../images/background.png')} alt={""}  />
-                    <p style={captionStyle}>{title}</p>
-					<div className="example">
-        
-        <button className="btn btn-action" /*style= {buttonStyle}*/ onClick={this.onOpenModal}>
-          View More
-        </button>{' '}
-        
-        <Modal
-          open={open}
-          onClose={this.onCloseModal}
-          little
-          classNames={{
-            transitionEnter: 'transition-enter',
-            transitionEnterActive: 'transition-enter-active', 
-            transitionExit: 'transition-exit-active',
-            transitionExitActive: 'transition-exit-active',
-          }}
-          animationDuration={1000}
-        >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-            hendrerit risus, sed porttitor quam.
-          </p>
-        </Modal>
-      </div>
-					
+                    <p>{title}</p>
                 </div>
         );
     }
 }
-
 
 export default displayItem;
