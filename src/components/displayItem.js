96bec7b713b48cf0560d73330ee0e1ce3bc6c6ea
unbornharry <unbornharry@yahoo.com>
2018-03-10 19:30:49 -0700

    CSS updates for displayItem Gather images and title for displayItem based on parameters passed displayItemList - updated attributeList for all available images Removed useless component from googleMap

--- src/components/displayItem.js
+++ src/components/displayItem.js
@@ -5,12 +5,12 @@ import '../../node_modules/react-responsive-modal/lib/react-responsive-modal.css
 
 
 const style = {
-    background: 'white',
+    background: 'linear-gradient(lavender, lightgrey)',
     display: 'inline-block',
     width: '200px',
     margin: '13px',
     padding: '5px',
-    height: '200px',
+    height: '230px',
     borderRadius: '10px',
     boxShadow: '0 4px 8px 0 #002B36, 0 6px 20px 0 #002B36',
     position: 'relative',
@@ -20,6 +20,7 @@ const style = {
 const buttonStyle = {
 	background: '#4CAF50', /* Green */
     border: 'none',
+    borderRadius: '4px',
     color: 'white',
     padding: '5px 10px',
     textAlign: 'center',
@@ -36,12 +37,18 @@ const buttonStyle = {
 const imageStyle = {
    maxWidth: '100%',
    maxHeight: '70%',
-    margin: 'auto'
+    margin: 'auto',
+    overflow: 'hidden'
 };
 
 const captionStyle = {
-    textAlign: 'center'
+    textAlign: 'center',
+    color: 'black',
+    fontSize: '14px',
+    fontWeight: 'bold'
 };
+
+var images = require.context('../images', true);
 
 class displayItem extends Component {
     state = {
@@ -59,81 +66,78 @@ class displayItem extends Component {
 
     render(){
 		const { open } = this.state;
-        let { image } = this.props;
-        console.log(image);
+        let { image, title } = this.props;
+        let img_src = images(image);
         return(
-                <div style={style}>
-                    <div style={imageStyle}>
-                        <img src={require('../images/fireExtinguishers/backupTrolleyFireExtinguishers.png')} style={imageStyle} alt={""}  />
-                    </div>
-					<div className="example">
-                        <p style={captionStyle}>
+            <div style={style}>
+                <div style={imageStyle}>
+                    <img src={img_src} style={imageStyle} alt=""  />
+                </div>
+                <div style={captionStyle}>{title}</div>
+                <div style={captionStyle}>
                     <button className="btn btn-action" style= {buttonStyle} onClick={this.onOpenModal}>
                       View More
-                    </button>{' '}</p>
-                    <Modal
-                      open={open}
-                      onClose={this.onCloseModal}
-                      little
-                      classNames={{
-                        transitionEnter: 'transition-enter',
-                        transitionEnterActive: 'transition-enter-active',
-                        transitionExit: 'transition-exit-active',
-                        transitionExitActive: 'transition-exit-active',
-                      }}
-                      animationDuration={300} closeIconSize = {18}
-                    >
-					
-					<table style={{color:'red'}}>
-						<tr>
-							<th colspan="2">Fire Extinguisher</th>
-						</tr>
-							<tr>
-								<td><img src={require('../images/Transp/Backup trolley mount fire extinguisher.png')}  /> </td>
-								<td><p style={{color:'red'}}>
-								Description:<br/><br/>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-									pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-									hendrerit risus, sed porttitor quam.
-									</p>
-								</td>				  
-							</tr>
-							<tr>
-								<td></td>
-								<td style={{color:'red'}}><p>Options:</p>
+                    </button>
+                </div>
+                <Modal
+                  open={open}
+                  onClose={this.onCloseModal}
+                  little
+                  classNames={{
+                    transitionEnter: 'transition-enter',
+                    transitionEnterActive: 'transition-enter-active',
+                    transitionExit: 'transition-exit-active',
+                    transitionExitActive: 'transition-exit-active',
+                  }}
+                  animationDuration={300} closeIconSize = {18}
+                >
+                <table style={{color:'red'}}>
+                    <tr>
+                        <th colspan="2">Fire Extinguisher</th>
+                    </tr>
+                        <tr>
+                            <td><img src={require('../images/fireExtinguishers/backupTrolleyFireExtinguishers.png')}  /> </td>
+                            <td><p style={{color:'red'}}>
+                            Description:<br/><br/>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                                pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+                                hendrerit risus, sed porttitor quam.
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td style={{color:'red'}}><p>Options:</p>
 
-								<form class="boxed">
-									<input type="radio" id="one" name="weight" value="1KG"/>
-									<label for="one">1KG</label>
-					
-									<input type="radio" id="two" name="weight" value="2KG"/>
-									<label for="two">2KG</label>
-					
-									<input type="radio" id="three" name="weight" value="3KG"/>
-									<label for="three">3KG</label>
-					
-									<input type="radio" id="Four" name="weight" value="4KG"/>
-									<label for="four">4KG</label>
-					
-									<input type="radio" id="Five" name="weight" value="5KG"/>
-									<label for="five">5KG</label>					
+                            <form class="boxed">
+                                <input type="radio" id="one" name="weight" value="1KG"/>
+                                <label for="one">1KG</label>
 
-									<br/><br/>
-									
-									<button style= {buttonStyle} name="callBack"> Request CallBack</button>
-									<button style= {buttonStyle} name="enquiry"> Send Enquiry</button>
-						
-								</form>
-								</td>
-							</tr>
-					</table>				
-                  
+                                <input type="radio" id="two" name="weight" value="2KG"/>
+                                <label for="two">2KG</label>
+
+                                <input type="radio" id="three" name="weight" value="3KG"/>
+                                <label for="three">3KG</label>
+
+                                <input type="radio" id="Four" name="weight" value="4KG"/>
+                                <label for="four">4KG</label>
+
+                                <input type="radio" id="Five" name="weight" value="5KG"/>
+                                <label for="five">5KG</label>
+
+                                <br/><br/>
+
+                                <button style= {buttonStyle} name="callBack"> Request CallBack</button>
+                                <button style= {buttonStyle} name="enquiry"> Send Enquiry</button>
+
+                            </form>
+                            </td>
+                        </tr>
+                    </table>
                 </Modal>
             </div>
-        </div>
         );
     }
 }
-
 
 export default displayItem;
--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => (
-  <div style={{
-    position: 'relative', display: 'inline-block', overflow:'visible', color: 'white', background: 'blue',
-    height: 40, width: 60, top: -20, left: -30,
-  }}>
-    {text}
-  </div>
-);
-
 const mapStyle = {
     width: '50%',
     height: '350px',
